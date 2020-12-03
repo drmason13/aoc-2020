c5c07692949b9e214b964db5482483258f6a1b56
drmason13 <drmason13@users.noreply.github.com>
2020-12-03 21:00:15 +0000

    day 3 complete

--- src/day3.rs
+++ src/day3.rs
@@ -1,14 +1,64 @@
-use aoc_runner_derive::{aoc_generator, aoc};
+use aoc_runner_derive::aoc;
 
-#[aoc_generator(day3)]
-pub fn input_generator(input: &str) -> Vec<u32> {
-    input.lines()
-        .map(|line| line.parse::<u32>().expect("non-integer in input!"))
-        .collect::<Vec<u32>>()
+fn check_slope(slope: (usize, usize), map: &Vec<&str>) -> usize {
+    let mut x = 0;
+    let mut y = 0;
+    let mut tree_count = 0;
+    let width = map[0].len();
+
+    while y < map.len() {
+        let is_tree = map[y].as_bytes()[x % width] == b'#';
+        if is_tree {
+            tree_count += 1;
+        }
+        x += slope.0;
+        y += slope.1
+    }
+    tree_count
 }
 
 #[aoc(day3, part1)]
-pub fn solve(_list: &Vec<u32>) -> Result<u32, &'static str> {
-    // ...
-    Ok(0)
+pub fn part1(input: &str) -> usize {
+    let map = input.lines().collect();
+    check_slope((3, 1), &map)
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> usize {
+    let map = input.lines().collect();
+
+    let mut answer = 1;
+    for slope in &[
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+    ] {
+        answer *= check_slope(*slope, &map);
+    }
+    answer
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const TEST_INPUT: &'static str = "\
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(part2(TEST_INPUT), 336);
+    }
 }
