58d793cbd284364be7fa47e50e2e723a4e800647
drmason13 <drmason13@users.noreply.github.com>
2020-12-11 19:09:50 +0000

    day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,35 +1,71 @@
-use aoc_runner_derive::{aoc_generator, aoc};
+use aoc_runner_derive::aoc;
 
-#[aoc_generator(day6)]
-pub fn input_generator(input: &str) -> Vec<u32> {
-    unimplemented!("")
-    // input.lines()
-    // ...
-}
+use std::collections::HashSet;
 
 #[aoc(day6, part1)]
-pub fn part1(list: &Vec<usize>) -> Result<usize, &'static str> {
-    unimplemented!("")
+pub fn part1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|group| {
+            let mut unique_answers = HashSet::new();
+            for person in group.lines() {
+                for answer in person.chars() {
+                    unique_answers.insert(answer);
+                }
+            }
+            unique_answers
+        })
+        .map(|x| x.len())
+        .sum()
 }
 
 #[aoc(day6, part2)]
-pub fn part2(list: &Vec<usize>) -> Result<usize, &'static str> {
-    unimplemented!("")
+pub fn part2(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|group| {
+            let mut group_iter = group.lines().map(|person| {
+                // each person is a set of answers
+                person.chars().collect::<HashSet<char>>()
+            });
+            // we want to return the intersecting answers of all the people in each group
+            let intersecting_answers = group_iter.next().unwrap();
+            group_iter.fold(intersecting_answers, |acc, set| {
+                acc.intersection(&set).cloned().collect()
+            })
+        })
+        .map(|x| x.len())
+        .sum()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    const TEST_INPUT: &'static str = "\
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
     #[test]
-    #[ignore]
     fn part1_works() {
-        assert_eq!(0, part1(&vec![1]).unwrap());
+        assert_eq!(11, part1(&TEST_INPUT));
     }
 
     #[test]
-    #[ignore]
     fn part2_works() {
-        assert_eq!(0, part2(&vec![2]).unwrap());
+        assert_eq!(6, part2(&TEST_INPUT));
     }
 }
